32f5869dbb72edda0e0b21325742f4034a0406dc
iza <shigemura0820@yahoo.co.jp>
2015-03-26 02:05:17 +0900

    明日

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,11 @@ module ApplicationHelper
 	def tokyotime(time)
 		time.strftime("%Y年%m月%d日 %H:%M")
 	end
+
+	def image_30_30(image)
+		image_name = image
+		img = Magick::ImageList.new(image)
+		new_img = img.scale(0.25)
+		new_img.write(image_name)
+	end
 end
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -14,7 +14,27 @@ class ImageUploader < CarrierWave::Uploader::Base
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+
   end
+
+  process convert: 'jpg'
+
+
+  # thumb バージョン(width 400px x height 200px)
+  version :thumb do
+    process :resize_to_fit => [100, 100]
+  end
+
+  # 許可する画像の拡張子
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
+
+  # 変換したファイルのファイル名の規則
+  def filename
+    "#{Time.now.strftime('%Y%m%d%H%M%S')}.jpg"
+  end
+
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
@@ -32,7 +52,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-     process :resize_to_fill => [50 , 50]
+     # process :resize_to_fill => [50 , 50]
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   # def extension_white_list
