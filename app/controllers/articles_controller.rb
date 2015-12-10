03318362ddaa59b78fe9120c18cbf3c9b3fd8f1c
Hiroki Shigemura <shigemura0820@yahoo.co.jp>
2015-12-10 18:20:45 +0900

    Update articles_controller.rb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,48 +1,48 @@
 class ArticlesController < ApplicationController
-	before_action :facebook
-	before_action :set_article, only:[:edit,:show,:update,:destroy,] 
-require "RMagick"
-
-	def index
-		@articles = Article.all.order("created_at ASC").includes(:user,:favorites)
-	end
-
-	def show
-	  @comments = @article.comments
-	  @comment = @article.comments.build
-	end
-
-	def new
-		@article = Article.new
-	end
-
-	def create
-		tmp_article_params = article_params
-		image_data = base64_conversion(tmp_article_params[:remote_image_url])
-		tmp_article_params[:image] = image_data
-		tmp_article_params[:remote_image_url] = nil
-		@article = Article.new(tmp_article_params)
-		if @article.save
-			redirect_to users_path 
-		else
-			render 'new'	
-		end
-	end
-
+  before_action :facebook
+  before_action :set_article, only:[:edit,:show,:update,:destroy,] 
+  require "RMagick"
+	
+  def index
+    @articles = Article.all.order("created_at ASC").includes(:user,:favorites)
+  end
+	
+  def show
+    @comments = @article.comments
+    @comment = @article.comments.build
+  end
+	
+  def new
+    @article = Article.new
+  end
+	
+  def create
+    tmp_article_params = article_params
+    image_data = base64_conversion(tmp_article_params[:remote_image_url])
+    tmp_article_params[:image] = image_data
+    tmp_article_params[:remote_image_url] = nil
+    @article = Article.new(tmp_article_params)
+    if @article.save
+      redirect_to users_path 
+    else
+      render 'new'	
+    end
+  end
+  
 	def destroy
 		@article.destroy
 		redirect_to :back
 	end
-
+	
 	private
 	def article_params
 		params[:article].permit(:title,:article,:user_id,:remote_image_url, :image)
 	end
-
+	
 	def comment_params
 		params[:comment].permit(:comment, :user_id)
 	end
-
+	
 	def set_article
 		@article = Article.find(params[:id])		
 	end	
@@ -51,7 +51,7 @@ require "RMagick"
 		token = "CAAWF8XmmjJEBAI2bTSdbm0UWBWcBgFB1DZCCGD6yM4ZAqYfjmyZB7JFyiQGSsFNZBmtw2LWWkz3qmbt7QmgZC8doxMyECRLRb6GpXGdhnXC7HIt9vFPZAB0bsGaKOqFco9HoKYFuSykHRbB6yHQWZC8LtRGOxssIxPywp5rwoGHKUPXLiTw6oYR3JzFzcAWO7fs5UsD4rhLa1OiR5Y3WDxd"
 		facebook = Koala::Facebook::API.new(token)
 	end
-
+	
 	def base64_conversion(uri_str, filename = 'base64')
 		image_data = split_base64(uri_str)
 		image_data_string = image_data[:data]
@@ -63,7 +63,7 @@ require "RMagick"
 		img_params = {:filename => "#{filename}.#{image_data[:extension]}", :type => image_data[:type], :tempfile => temp_img_file}
 		ActionDispatch::Http::UploadedFile.new(img_params)
 	end
-
+	
 	def split_base64(uri_str)
 		if uri_str.match(%r{data:(.*?);(.*?),(.*)$})
 			uri = Hash.new
